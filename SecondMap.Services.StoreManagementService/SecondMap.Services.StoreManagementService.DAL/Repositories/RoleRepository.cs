c890627c50b9aee0633b1792157ca9cc6c13f933
glebtyanov <pont.gleb@gmail.com>
2023-07-18 16:51:08 +0300

    Added generic repositories and service registrations

---
+++ SecondMap.Services.StoreManagementService/SecondMap.Services.StoreManagementService.DAL/Repositories/RoleRepository.cs
@@ -0,0 +1,13 @@
+﻿using SecondMap.Services.StoreManagementService.DAL.Context;
+using SecondMap.Services.StoreManagementService.DAL.Interfaces;
+using SecondMap.Services.StoreManagementService.DAL.Models;
+
+namespace SecondMap.Services.StoreManagementService.DAL.Repositories
+{
+	internal class RoleRepository : GenericRepository<Role>, IRoleRepository
+	{
+		public RoleRepository(StoreManagementDbContext dbContext) : base(dbContext)
+		{
+		}
+	}
+}
