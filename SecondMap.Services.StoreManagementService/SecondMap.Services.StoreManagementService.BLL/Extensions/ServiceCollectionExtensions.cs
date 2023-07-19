171d369360ae236915aca297f2cd1caf5316128a
glebtyanov <pont.gleb@gmail.com>
2023-07-19 11:32:09 +0300

    feature/nmd-394 added basic services and controllers

---
+++ SecondMap.Services.StoreManagementService/SecondMap.Services.StoreManagementService.BLL/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SecondMap.Services.StoreManagementService.BLL.Interfaces;
+using SecondMap.Services.StoreManagementService.BLL.Services;
+
+namespace SecondMap.Services.StoreManagementService.DAL.Extensions
+{
+	public static class ServiceCollectionExtensions
+	{
+		public static IServiceCollection AddBusinessLogicLayerServiceCollection(this IServiceCollection services)
+		{
+			services.AddScoped<IReviewService, ReviewService>();
+			services.AddScoped<IScheduleService, ScheduleService>();
+			services.AddScoped<IRoleService, RoleService>();
+			services.AddScoped<IStoreService, StoreService>();
+			services.AddScoped<IUserService, UserService>();
+
+			return services;
+		}
+	}
+}
