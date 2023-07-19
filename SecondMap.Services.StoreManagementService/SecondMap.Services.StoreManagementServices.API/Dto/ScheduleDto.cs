08c2209dfba01eb2930c4084db8b896a8decf63c
glebtyanov <pont.gleb@gmail.com>
2023-07-19 13:05:04 +0300

    feature/nmd-394 added services, controllers, started adding dto

---
+++ SecondMap.Services.StoreManagementService/SecondMap.Services.StoreManagementServices.API/Dto/ScheduleDto.cs
@@ -0,0 +1,13 @@
+﻿using SecondMap.Services.StoreManagementService.DAL.Enums;
+
+namespace SecondMap.Services.StoreManagementService.API.Dto
+{
+	public class ScheduleDto : BaseDto
+	{
+		public int StoreId { get; set; }
+		public DayOfWeekEu Day { get; set; }
+		public TimeOnly OpeningTime { get; set; }
+		public TimeOnly ClosingTime { get; set; }
+		public bool IsClosed { get; set; }
+	}
+}
