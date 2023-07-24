3a9ff6fe0cd18d12a4ca31e37a52de5e519f144a
glebtyanov <85569847+glebtyanov@users.noreply.github.com>
2023-07-24 11:57:28 +0300

    NMD-414 Implemented model validation (#13)

---
+++ SecondMap.Services.StoreManagementService/SecondMap.Services.StoreManagementServices.API/Helpers/Validators/StoreValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using SecondMap.Services.StoreManagementService.API.ViewModels;
+
+namespace SecondMap.Services.StoreManagementService.API.Helpers.Validators
+{
+	public class StoreValidator : AbstractValidator<StoreViewModel>
+	{
+		public StoreValidator()
+		{
+			RuleFor(s => s.Name).NotEmpty().MaximumLength(256);
+			RuleFor(s => s.Address).NotEmpty().MaximumLength(256);
+			RuleFor(s => s.Price).NotEmpty();
+		}
+	}
+}
---
+++ SecondMap.Services.StoreManagementService/SecondMap.Services.StoreManagementServices.API/Helpers/Validators/UsersValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using SecondMap.Services.StoreManagementService.API.ViewModels;
+
+namespace SecondMap.Services.StoreManagementService.API.Helpers.Validators
+{
+	public class UsersValidator : AbstractValidator<UserViewModel>
+	{
+		public UsersValidator()
+		{
+			RuleFor(u => u.Username).NotEmpty().MaximumLength(20);
+			RuleFor(u => u.Password).NotEmpty().MinimumLength(8).MaximumLength(32);
+		}
+	}
+}
