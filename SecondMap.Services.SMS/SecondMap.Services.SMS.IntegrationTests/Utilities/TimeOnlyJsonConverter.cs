3429d871c1ca06e1bbcaede718cd59dddacf91ed
glebtyanov <pont.gleb@gmail.com>
2023-07-31 13:57:34 +0300

    nmd-424 previous commit

--- SecondMap.Services.SMS/SecondMap.Services.SMS.IntegrationTests/Utilities/TimeOnlyJsonConverter.cs
+++ SecondMap.Services.SMS/SecondMap.Services.SMS.IntegrationTests/Utilities/TimeOnlyJsonConverter.cs
@@ -1,14 +1,17 @@
-﻿public class TimeOnlyJsonConverter : JsonConverter<TimeOnly>
+﻿namespace SecondMap.Services.SMS.IntegrationTests.Utilities
 {
-	private const string TimeFormat = "HH:mm:ss.FFFFFFF";
-
-	public override TimeOnly ReadJson(JsonReader reader, Type objectType, TimeOnly existingValue, bool hasExistingValue, JsonSerializer serializer)
+	public class TimeOnlyJsonConverter : JsonConverter<TimeOnly>
 	{
-		return TimeOnly.ParseExact((string)reader.Value!, TimeFormat, CultureInfo.InvariantCulture);
-	}
+		private const string TimeFormat = "HH:mm:ss.FFFFFFF";
 
-	public override void WriteJson(JsonWriter writer, TimeOnly value, JsonSerializer serializer)
-	{
-		writer.WriteValue(value.ToString(TimeFormat, CultureInfo.InvariantCulture));
+		public override TimeOnly ReadJson(JsonReader reader, Type objectType, TimeOnly existingValue, bool hasExistingValue, JsonSerializer serializer)
+		{
+			return TimeOnly.ParseExact((string)reader.Value!, TimeFormat, CultureInfo.InvariantCulture);
+		}
+
+		public override void WriteJson(JsonWriter writer, TimeOnly value, JsonSerializer serializer)
+		{
+			writer.WriteValue(value.ToString(TimeFormat, CultureInfo.InvariantCulture));
+		}
 	}
 }
