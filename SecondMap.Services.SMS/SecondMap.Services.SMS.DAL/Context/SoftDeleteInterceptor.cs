90e307aa822d82aa0b1285fafaf3185a36496962
glebtyanov <pont.gleb@gmail.com>
2023-08-04 15:32:41 +0300

    nmd-442 added interceptor and interface for it

---
+++ SecondMap.Services.SMS/SecondMap.Services.SMS.DAL/Context/SoftDeleteInterceptor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondMap.Services.SMS.DAL.Context
+{
+	internal class SoftDeleteInterceptor
+	{
+	}
+}
---
+++ SecondMap.Services.SMS/SecondMap.Services.SMS.DAL/Interfaces/ISoftDeletable.cs
@@ -0,0 +1,20 @@
+﻿namespace SecondMap.Services.SMS.DAL.Interfaces
+{
+	public interface ISoftDeletable
+	{
+		public bool IsDeleted { get; set; }
+		public DateTimeOffset? DeletedAt { get; set; }
+
+		public void Undo()
+		{
+			IsDeleted = false;
+			DeletedAt = DateTimeOffset.MinValue;
+		}
+
+		public void SoftDelete()
+		{
+			IsDeleted = true;
+			DeletedAt = null;
+		}
+	}
+}
