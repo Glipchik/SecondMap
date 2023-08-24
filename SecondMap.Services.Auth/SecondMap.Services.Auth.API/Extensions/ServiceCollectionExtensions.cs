ea4a2656afebdc7e4369fbfa80f33e67fcd381fd
glebtyanov <pont.gleb@gmail.com>
2023-08-24 10:42:12 +0300

    nmd-437 auth logic

---
+++ SecondMap.Services.Auth/SecondMap.Services.Auth.API/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using SecondMap.Services.Auth.API.RequestModels;
+using SecondMap.Services.Auth.API.Validators;
+
+namespace SecondMap.Services.Auth.API.Extensions
+{
+	public static class ServiceCollectionExtensions
+	{
+		public static void AddValidations(this IServiceCollection services)
+		{
+			services.AddScoped<IValidator<LoginRequestModel>, LoginValidator>();
+			services.AddScoped<IValidator<RegisterRequestModel>, RegisterValidator>();
+		}
+	}
+}
